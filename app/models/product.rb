009056a99c398d4adc4e295f2737c5726704b489
sebastian <sebastian0092@gmail.com>
2015-02-23 00:15:12 -0500

    finished_catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,8 @@ class Product < ActiveRecord::Base
             }
   validates :title, length: { minimum: 10,
                               message: 'must be at least 10 characters long.' }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
